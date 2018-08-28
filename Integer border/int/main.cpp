8c1bc23e0b00b1a49733399f948b958af6d19b17
Nikcname <ferrux.nebiyev1@gmail.com>
2018-08-28 22:11:50 +0400

    borders

---
+++ Integer border/int/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+typedef int64_t saw;
+
+int main(int argc, char **argv)
+{
+	saw ii{9223000000000000000};
+    
+    std::cout<<ii<<std::endl;
+    
+	return 0;
+}
+// 111 1+2+2*2 = 7
+// 000 2*2*2*2 = 8
